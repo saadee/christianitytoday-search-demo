947c20b85e2e3c3da980c16b70feba97fbb7ff64
saadee <saadee.workspace@gmail.com>
2024-04-16 00:07:31 +0500

    color change

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -56,8 +56,8 @@ const LoginPage = ({ setAuthenticate, setLoading, loading }) => {
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
         <div>
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-[#647a1c]">
-            Ligonier Demo
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-[#d51b1e]">
+            Christianity Today Demo
           </h2>
           <h2 className="mt-1 text-center text-lg font-extrabold text-gray-400">
             Sign in to your account
@@ -142,7 +142,7 @@ const LoginPage = ({ setAuthenticate, setLoading, loading }) => {
             <button
               type="submit"
               disabled={loading}
-              className={`group relative w-full min-h-10 flex justify-center py-2 px-4 border border-transparent text-sm focus:ring-[#859e3b] bg-[#859e3b] hover:bg-[#647a1c] font-medium rounded-md text-white ${
+              className={`group relative w-full min-h-10 flex justify-center py-2 px-4 border border-transparent text-sm focus:ring-white bg-[#d51b1e] hover:bg-[#d51b1e] font-medium rounded-md text-white ${
                 loading && "cursor-not-allowed"
               } focus:outline-none focus:ring-2 focus:ring-offset-2`}
             >
